a456b5c0616a3fc9ebfc0f4720010c8e7c20db13
Joel Kerfoot <joelkerfoot@gmail.com>
2020-01-29 18:30:11 -0800

    updated assignment 1

---
+++ assignment_1/decision_tree/main.py
@@ -0,0 +1,106 @@
+# Refernece: http://bit.ly/2vycpoo
+
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.externals.six import StringIO 
+from IPython.display import Image
+import matplotlib.pyplot as plt
+import pydotplus, pickle, sys
+
+def get_best_tree(data, criterion, name="", graph=False):
+
+  x_train, y_train = data["training_data"], data["training_target"]
+  x_test, y_test = data["test_data"], data["test_target"]
+  
+  clf = DecisionTreeClassifier(random_state=0, criterion=criterion)
+  path = clf.cost_complexity_pruning_path(x_train, y_train)
+  ccp_alphas, impurities = path.ccp_alphas, path.impurities
+
+  clfs = []
+  for ccp_alpha in ccp_alphas:
+    clf = DecisionTreeClassifier(random_state=0, criterion=criterion, ccp_alpha=ccp_alpha)
+    clf.fit(x_train, y_train)
+    clfs.append(clf)
+
+  # Remove tree with only one node
+  clfs = clfs[:-1]
+  ccp_alphas = ccp_alphas[:-1]
+
+  train_scores = [clf.score(x_train, y_train) for clf in clfs]
+  test_scores = [clf.score(x_test, y_test) for clf in clfs]
+  node_counts = [clf.tree_.node_count for clf in clfs]
+  depth = [clf.tree_.max_depth for clf in clfs]
+
+  if graph:
+    # Graph num nodes and depth vs alpha
+    fig, ax = plt.subplots(2, 1)
+    ax[0].plot(ccp_alphas, node_counts, marker='o', drawstyle="steps-post")
+    ax[0].set_xlabel("alpha")
+    ax[0].set_ylabel("number of nodes")
+    ax[0].set_title("Number of nodes vs alpha")
+    ax[1].plot(ccp_alphas, depth, marker='o', drawstyle="steps-post")
+    ax[1].set_xlabel("alpha")
+    ax[1].set_ylabel("depth of tree")
+    ax[1].set_title("Depth vs alpha")
+    fig.tight_layout()
+
+    plot_name = "images/plot0_" + criterion + ".png" if name == "" else "images/" + name + "_plot0_" + criterion + ".png"
+    plt.savefig(plot_name)
+
+    # Graph accuracy vs alpha
+    fig, ax = plt.subplots()
+    ax.set_xlabel("alpha")
+    ax.set_ylabel("accuracy")
+    ax.set_title("Accuracy vs alpha for training and testing sets")
+    ax.plot(ccp_alphas, train_scores, marker='o', label="train",
+            drawstyle="steps-post")
+    ax.plot(ccp_alphas, test_scores, marker='o', label="test",
+            drawstyle="steps-post")
+    ax.legend()
+
+    plot_name = "images/plot1_" + criterion + ".png" if name == "" else "images/" + name + "_plot1_" + criterion + ".png"
+    plt.savefig(plot_name)
+
+  return clfs[test_scores.index(max(test_scores))]
+
+def visualize_tree(tree, feature_names, class_names, criterion, name=""):
+  dot_data = StringIO()
+  export_graphviz(
+    tree, 
+    out_file=dot_data, 
+    filled=True, 
+    rounded=True, 
+    special_characters=True, 
+    feature_names=feature_names, 
+    class_names=class_names
+  )
+
+  graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+  image = graph.create_png()
+
+  plot_name = "images/plot2_" + criterion + ".png" if name == "" else "images/" + name + "_plot2_" + criterion + ".png"
+
+  with open(plot_name, 'wb') as f:
+    f.write(image)
+
+def main():
+  if len(sys.argv) < 2:
+    print('Usage: python main.py <file_name>')
+    exit()
+
+  file_name = sys.argv[1]
+  graph = '-g' in sys.argv
+
+  data = None
+  with open('data/' + file_name , 'rb') as f:
+    data = pickle.load(f)
+
+  entropy_tree = get_best_tree(data, criterion="entropy", graph=graph)
+  gini_tree = get_best_tree(data, criterion="gini", graph=graph)
+
+  if graph:
+    feature_names, class_names = data["feature_names"], data["class_names"]
+    visualize_tree(entropy_tree, feature_names, class_names, "entropy")
+    visualize_tree(gini_tree, feature_names, class_names, "gini")
+
+if __name__ == "__main__":
+  main()
--- assignment_1/process_data.py
+++ assignment_1/process_data.py
@@ -1,20 +1,53 @@
-import sys, csv
+import sys, csv, random, pickle
 
-if len(sys.argv) < 4:
+def format_data(unformated):
+  data, target = [], []
+  for row in unformated:
+    data.append(row[:-1])
+    target.append(min(int(row[-1]), 1))
+
+  return data, target
+  
+
+if len(sys.argv) < 3:
   print('Expected 3 arguments got {}'.format(len(sys.argv)))
-  print('Usage: python process_data.py <training set %> <test set%> <output file name>')
-  quit()
+  print('Usage: python process_data.py <training set %> <output file name>')
+  exit()
 
-training = float(sys.argv[1])
-test = float(sys.argv[2])
-file_name = sys.argv[3]
+tr_percent = float(sys.argv[1])
+file_name = sys.argv[2]
 
+data = None
 with open('processed.cleveland.data', 'r') as f:
   reader = csv.reader(f)
-  data = list(reader)
+  temp = list(reader)
 
-  for row in data:
+  for row in temp:
     if '?' in row:
-      data.remove(row)
+      temp.remove(row)
 
-  
+  data = [[float(x) for x in row] for row in temp]
+
+test = []
+for i in range(int((1 - tr_percent) * len(data))):
+  test.append(data.pop(random.randint(0, len(data) - 1)))
+
+training_data, training_target = format_data(data)
+test_data, test_target = format_data(test)
+
+names = None
+with open('processed.cleveland.names', 'r') as f:
+  reader = csv.reader(f)
+  names = list(reader)
+
+d = {
+  "training_data": training_data,
+  "training_target": training_target,
+  "test_data": test_data,
+  "test_target": test_target,
+  "feature_names": names[0],
+  "class_names": names[1]
+}
+
+with open('data/' + file_name, 'wb') as f:
+  pickle.dump(d, f)
